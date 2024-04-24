857e9d8069e548e84119ef8c722b9c702b1bd65c
Mekal <23715925+MekalBoy@users.noreply.github.com>
2024-04-24 23:56:22 +0300

    Update join again

--- src/main/scala/Database.scala
+++ src/main/scala/Database.scala
@@ -16,44 +16,42 @@ case class Database(tables: List[Table]) {
       None
   }
 
-  def join (table1: String, c1: String, table2: String, c2: String): Option[Table] = {
+  def join(table1: String, c1: String, table2: String, c2: String): Option[Table] = {
     val firstTable = tables.find(_.name == table1)
     val secondTable = tables.find(_.name == table2)
 
     (firstTable, secondTable) match {
-      case (Some(table1), Some(table2)) => {
-        val tableData1 = table1.data
-        val tableData2 = table2.data.map(row => row - c2 + (c1 -> row(c2)))
+      case (Some(t1), Some(t2)) => {
+        val td1 = t1.data
+        val td2 = t2.data.map(row => row - c2 + (c1 -> row(c2)))
 
-        def aux(row1: Row, acc: Tabular): Tabular =
-          tableData2.filter(row2 => row2(c1) contains row1(c1)) match {
-            case row2 :: _ => {
-              def merge(pairs1: List[(String, String)], pairs2: List[(String, String)]): List[(String, String)] =
-                (pairs1, pairs2) match {
-                  case (x :: xs, y :: ys) =>
-                    if x(0) != y(0) then merge(pairs1, ys)
-                    else (if x(1) == y(1) then x else (x(0), x(1) + ';' + y(1))) :: merge(xs, row2.toList)
-                  case (x :: xs, _) => x :: merge(xs, row2.toList)
-                  case (_, y :: ys) =>
-                    if row1 contains y(0) then merge(Nil, ys)
-                    else y :: merge(Nil, ys)
-                  case (_, _) => Nil
-                }
-              merge(row1.toList, row2.toList).toMap :: acc
+        def tuple(r1: Row, acc: List[(Row, Row)]): List[(Row, Row)] =
+          td2.filter(_(c1) contains r1(c1)) match
+            case Nil => acc
+            case r2 :: _ => (r1, r2) :: acc
+
+        def merge(r1: Row, r2: Row): Row = {
+          def helper(tuple: (String, String), acc: List[(String, String)]): List[(String, String)] =
+            r2.get(tuple._1) match {
+              case Some(str) => if str == tuple._2 then (tuple._1, str) :: acc else (tuple._1, tuple._2 + ';' + str) :: acc
+              case _ => tuple :: acc
             }
-            case _ => acc
-          }
 
-        val header = (tableData1.head ++ tableData2.head).map(_(0) -> "")
+          r1.foldRight(Nil)(helper).toMap ++ r2.filterNot(r1 contains _(0))
+        }
 
-        val part1 = tableData1.foldRight(Nil)(aux)
-        val part2 = tableData1.filterNot(part1.map(_(c1)) contains _(c1))
-        val part3 = tableData2.filterNot(part1.map(_(c1)) contains _(c1))
+//        def replace(tuple: (String, String)): (String, String) = if tuple._1 != c2 then tuple else (c1, tuple._2)
 
-        Some(Table(table1.name, (part1 ::: part2 ::: part3).map(header ++ _)))
+        val header = (td1.head ++ td2.head).map(_(0) -> "")
+
+        val merged = td1.foldRight(Nil)(tuple).map(merge)
+        val first = td1.filterNot(merged.map(_(c1)) contains _(c1))
+        val second = td2.filterNot(merged.map(_(c1)) contains _(c1))
+
+        Some(Table(t1.name, (merged ::: first ::: second).map(header ++ _)))
       }
-      case (Some(table1), _) => Some(table1)
-      case (_, Some(table2)) => Some(table2)
+      case (Some(t1), _) => Some(t1)
+      case (_, Some(t2)) => Some(t2)
       case (_, _) => None
     }
   }
